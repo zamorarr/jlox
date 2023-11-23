863348bfda91a9f19f54d33685a5c9703d774584
Robert Zamora <zamora.rr@gmail.com>
2023-11-23 12:28:09 -0500

    4.5 Recognizing Lexemes

--- src/com/craftinginterpreters/lox/Scanner.java
+++ src/com/craftinginterpreters/lox/Scanner.java
@@ -3,8 +3,6 @@ package com.craftinginterpreters.lox;
 import java.util.ArrayList;
 import java.util.List;
 
-import static com.craftinginterpreters.lox.TokenType.*;
-
 class Scanner {
   private final String source;
   private final List<Token> tokens = new ArrayList<>();
@@ -24,11 +22,65 @@ class Scanner {
     }
 
     // add EOF token
-    tokens.add(new Token(EOF, "", null, line));
+    tokens.add(new Token(TokenType.EOF, "", null, line));
     return tokens;
+  }
+
+  private void scanToken() {
+    char c = advance();
+    switch (c) {
+      case '(': addToken(TokenType.LEFT_PAREN); break;
+      case ')': addToken(TokenType.RIGHT_PAREN); break;
+      case '{': addToken(TokenType.LEFT_BRACE); break;
+      case '}': addToken(TokenType.RIGHT_BRACE); break;
+      case ',': addToken(TokenType.COMMA); break;
+      case '.': addToken(TokenType.DOT); break;
+      case '-': addToken(TokenType.MINUS); break;
+      case '+': addToken(TokenType.PLUS); break;
+      case ';': addToken(TokenType.SEMICOLON); break;
+      case '*': addToken(TokenType.STAR); break;
+      case '!':
+        addToken(match('=') ? TokenType.BANG_EQUAL : TokenType.BANG);
+        break;
+      case '=':
+        addToken(match('=') ? TokenType.EQUAL_EQUAL : TokenType.EQUAL);
+        break;
+      case '<':
+        addToken(match('=') ? TokenType.LESS_EQUAL : TokenType.LESS);
+        break;
+      case '>':
+        addToken(match('=') ? TokenType.GREATER_EQUAL : TokenType.GREATER);
+        break;
+
+      default:
+        Lox.error(line, "Unexpected character.");
+        break;
+    }
+  }
+
+  private boolean match(char expected) {
+    if (isAtEnd()) return false;
+    if (source.charAt(current) != expected) return false;
+
+    // if match, consume character
+    current++;
+    return true;
   }
 
   private boolean isAtEnd() {
     return current >= source.length();
+  }
+
+  private char advance() {
+    return source.charAt(current++);
+  }
+
+  private void addToken(TokenType type) {
+    addToken(type, null);
+  }
+
+  private void addToken(TokenType type, Object literal) {
+    String text = source.substring(start, current);
+    tokens.add(new Token(type, text, literal, line));
   }
 }
