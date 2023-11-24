0816c5cf8f283b902a4afa7ba701353daeff0724
Robert Zamora <zamora.rr@gmail.com>
2023-11-23 21:45:38 -0500

    4.7 Reserved Words and Identifiers

--- src/com/craftinginterpreters/lox/Scanner.java
+++ src/com/craftinginterpreters/lox/Scanner.java
@@ -1,7 +1,9 @@
 package com.craftinginterpreters.lox;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 class Scanner {
   private final String source;
@@ -9,6 +11,27 @@ class Scanner {
   private int start = 0;
   private int current = 0;
   private int line = 0;
+
+  private static final Map<String, TokenType> keywords;
+  static {
+    keywords = new HashMap<>();
+    keywords.put("and",     TokenType.AND);
+    keywords.put("class",   TokenType.CLASS);
+    keywords.put("else",    TokenType.ELSE);
+    keywords.put("false",   TokenType.FALSE);
+    keywords.put("for",     TokenType.FOR);
+    keywords.put("fun",     TokenType.FUN);
+    keywords.put("if",      TokenType.IF);
+    keywords.put("nil",     TokenType.NIL);
+    keywords.put("or",      TokenType.OR);
+    keywords.put("print",   TokenType.PRINT);
+    keywords.put("return",  TokenType.RETURN);
+    keywords.put("super",   TokenType.SUPER);
+    keywords.put("this",    TokenType.THIS);
+    keywords.put("true",    TokenType.TRUE);
+    keywords.put("var",     TokenType.VAR);
+    keywords.put("while",   TokenType.WHILE);
+  }
 
   Scanner(String source) {
     this.source = source;
@@ -79,11 +102,23 @@ class Scanner {
       default:
         if (isDigit(c)) {
           number();
+        } else if (isAlpha(c)) {
+          identifier();
         } else {
           Lox.error(line, "Unexpected character.");
         }
         break;
     }
+  }
+
+  private void identifier() {
+    while (isAlphaNumeric(peek())) advance();
+
+    // check if identifier is a reserved keyword
+    String text = source.substring(start, current);
+    TokenType type = keywords.get(text);
+    if (type == null) type = TokenType.IDENTIFIER;
+    addToken(type);
   }
 
   private void number() {
@@ -136,6 +171,16 @@ class Scanner {
   private char peekNext() {
     if (current + 1 >= source.length()) return '\0';
     return source.charAt(current + 1);
+  }
+
+  private boolean isAlpha(char c) {
+    return (c >= 'a' && c <= 'z') ||
+           (c >= 'A' && c <= 'Z') ||
+           (c == '_');
+  }
+
+  private boolean isAlphaNumeric(char c) {
+    return isAlpha(c) || isDigit(c);
   }
 
   private boolean isDigit(char c) {
