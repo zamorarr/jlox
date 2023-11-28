d12ea0f520cbad43e0aa59951b0dc7e075ca3766
Robert Zamora <zamora.rr@gmail.com>
2023-11-27 21:24:01 -0500

    5.3 Working with Trees

--- src/com/craftinginterpreters/lox/Token.java
+++ src/com/craftinginterpreters/lox/Token.java
@@ -6,6 +6,8 @@ class Token {
   final Object literal;
   final int line;
 
+  // note only literals pass a value for the literal param
+  // other tokens just pass null
   Token(TokenType type, String lexeme, Object literal, int line) {
     this.type = type;
     this.lexeme = lexeme;
